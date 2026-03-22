1a363bbaf5ac0b1b09b26fa834a28359bb0a3ac6
SID AG <agsid@gmail.com>
2026-03-22 14:44:47 -0400

    yo

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,3 @@
 import PocketBase from 'pocketbase';
 
-// Replace with your actual server URL
 export const pb = new PocketBase('https://pb.robotics-catalyst.org');
-
-// Optional: Disable auto-cancellation if you have multiple 
-// simultaneous fetches on one page
